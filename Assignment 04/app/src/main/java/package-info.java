45c80a894f86944396afd1f10480fb626555fe63
Alexander Lotz <alexandermlotz@gmail.com>
2021-01-27 15:10:27 -0500

    Add Gradle task for execution of main class from CLI.

---
+++ Assignment 04/app/src/main/java/package-info.java
@@ -0,0 +1,8 @@
+// File:    App.java
+// Project: CSIS3101 Assignment 04
+// Author:  Alexander Lotz
+// History: Version 1.0 January 26, 2021
+
+/**
+ * Package containing Reflection implemntation of JUnit 5 tests.
+ */
