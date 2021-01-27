db1eff9c3e7c6c4d6bb4841f146a49e8e0340fcb
Alexander Lotz <lotal01@nova.edu>
2021-01-27 00:57:04 -0500

    Begin Gradle integration. Integrate checkstyle. Add support for Eclipse, IntelliJ, and VSCode. Begin dynamic class testing with Reflection. Staging tests on Assignment 04.

---
+++ Assignment 04/app/src/main/java/coursetests/package-info.java
@@ -0,0 +1,9 @@
+// File:    App.java
+// Project: CSIS3101 Assignment 04
+// Author:  Alexander Lotz
+// History: Version 1.0 January 26, 2021
+
+/**
+ * Package containing Reflection implemntation of JUnit 5 tests.
+ */
+package coursetests;
